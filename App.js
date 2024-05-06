989dac2718ed00045c50469e2f876b3d6efd0748
harishakaharu <harish00singh@gmail.com>
2024-05-06 21:54:55 +0530

    Initial commit
    Generated by create-expo-app 2.3.1.

---
+++ App.js
@@ -0,0 +1,5 @@
+import appStyles from "./styles/App.style";
+
+export default function App() {
+  return <View style={appStyles.main}></View>;
+}
---
+++ styles/App.style.js
@@ -0,0 +1,5 @@
+import { StyleSheet } from "react-native";
+const appStyles = StyleSheet.create({
+  main: {},
+});
+export default appStyles;
